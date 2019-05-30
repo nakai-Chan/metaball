46072e2050c2eee0e4e4e4cf4810142d5bb17e3c
NakaiKota <active3min.tk@gmail.com>
2019-05-30 14:15:41 +0900

    first commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MoveScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        x = (float)(0.1 * x);
+        y = (float)(0.1 * y);
+        transform.position += new Vector3(x, y);
+
+    }
+}
